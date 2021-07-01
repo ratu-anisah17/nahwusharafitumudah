7ec119e99d2d4ee7ec5c37b068e8ba19d7bd3d75
satrianifirdaus <86549109+satrianifirdaus@users.noreply.github.com>
2021-07-01 19:40:43 +0700

    Update navbar.php

--- app/Views/layout/navbar.php
+++ app/Views/layout/navbar.php
@@ -8,6 +8,7 @@
             <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                 <div class="navbar-nav">
                     <a class="nav-link active" aria-current="page" href="/">Daftar Ust</a>
+                    <a class="nav-link active" aria-current="page" href="/siswa">Daftar Santri</a>
                     <a class="nav-link" href='#'>About</a>
                     <a class="nav-link" href='#'>Contact</a>
                 </div>
